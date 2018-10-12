d0cddffbbb5e99bdb7473c0a088d9cf11ffed9e0
denislavdenov <42144642+denislavdenov@users.noreply.github.com>
2018-10-12 15:12:02 +0300

    Create servers.tf

---
+++ servers.tf
@@ -0,0 +1,36 @@
+variable "ami" {}
+variable "instance_type" {}
+variable "identity" {}
+variable "public_key" {}
+variable "security_group_id" {}
+
+resource "aws_key_pair" "training" {
+  key_name   = "${var.identity}"
+  public_key = "${var.public_key}"
+}
+
+module "randomname" {
+  source = "github.com/denislavdenov/terraform-local-randomname"
+}
+
+resource "aws_instance" "example" {
+  tags {
+    Name = "${module.randomname.instance_id}"
+  }
+
+  ami                    = "${var.ami}"
+  instance_type          = "${var.instance_type}"
+  key_name               = "${aws_key_pair.training.id}"
+  vpc_security_group_ids = ["${var.security_group_id}"]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update"
+    ]
+  }
+
+  connection {
+    user        = "ubuntu"
+    private_key = "${file("id_rsa")}"
+  }
+}
