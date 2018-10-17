f917581be767ec02278f146bd4d79a55443851e1
Denislav Denov <denislavdenov@Denislavs-MacBook-Pro.local>
2018-10-17 15:14:34 +0300

    add test

---
+++ test/integration/default/controls/check_pkg_spec.rb
@@ -0,0 +1,16 @@
+control 'check_pkg' do
+
+  describe package('nginx') do
+    it { should be_installed }
+  end
+
+  describe package('curl') do
+    it { should be_installed }
+  end
+
+  describe service('nginx') do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "ami" {}
+
+variable "instance_type" {}
+
+variable "identity" {}
+
+variable "security_group_id" {}
